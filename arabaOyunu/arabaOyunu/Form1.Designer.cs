f0bef071c4e192adb5b63095ec958e6653c356aa
AhmetDemir67 <147312042+AhmetDemir67@users.noreply.github.com>
2024-11-29 20:09:40 +0300

    Add files via upload

---
+++ arabaOyunu/arabaOyunu/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace arabaOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainPanel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.mainPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPanel1
+            // 
+            this.mainPanel1.Controls.Add(this.pictureBox3);
+            this.mainPanel1.Controls.Add(this.pictureBox2);
+            this.mainPanel1.Controls.Add(this.pictureBox1);
+            this.mainPanel1.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel1.Name = "mainPanel1";
+            this.mainPanel1.Size = new System.Drawing.Size(396, 630);
+            this.mainPanel1.TabIndex = 0;
+            this.mainPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.mainPanel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::arabaOyunu.Properties.Resources.png_transparent_taxi_car_simulator_3d_dodge_sprite_sprite_s_rectangle_car_video_game_thumbnail;
+            this.pictureBox1.Location = new System.Drawing.Point(143, 427);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(356, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 627);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 627);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 626);
+            this.Controls.Add(this.mainPanel1);
+            this.Name = "Form1";
+            this.Text = "Formula1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.mainPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel mainPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ arabaOyunu/arabaOyunu/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arabaOyunu
+{
+    public partial class Form1 : Form
+    {
+        // Araba ile ilgili durumları saklayan değişkenler
+        bool arabaCalısıyorMu = false;  // Araba çalışıyor mu?
+        int arabaHızı = 0;  // Araba hızını tutar
+        bool arabaDuruyorMu = true;  // Araba duruyor mu?
+        bool arabaLeft = false;  // Araba sola gitmekte mi?
+        bool arabaRight = false;  // Araba sağa gitmekte mi?
+        bool arabaBreak = false;  // Araba fren yapıyor mu?
+
+        // Rakip arabaların hareket yönlerini tutan diziler
+        bool[] RivalCarsLeft = { false, false, false };
+        bool[] RivalCarsRight = { false, false, false };
+
+        // Rakip arabaların görsellerini tutan bir dizi
+        PictureBox[] pictureBoxes = new PictureBox[3];
+        Image[] RivalsCarsImages = new Image[] { Properties.Resources.images, Properties.Resources.images__1_, Properties.Resources.pngtree_blue_car_top_view_png_image_6564078 };
+
+        // Random nesnesi
+        Random ran = new Random();
+
+        // Form oluşturulurken yapılacak işlemler
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Rakip arabaları oluşturur
+            CreateRivals();
+        }
+
+        // Rakip arabaları oluşturma fonksiyonu
+        public void CreateRivals()
+        {
+            // 3 adet rakip araba oluşturuyoruz
+            for (int i = 0; i < 3; i++)
+            {
+                // Yeni bir PictureBox nesnesi (rakip araba) oluşturuluyor
+                var rivalcar = new PictureBox();
+                pictureBoxes[i] = rivalcar;
+                rivalcar.Name = "Rivalcar" + (i + 1).ToString() + "_pictureBox1";  // Resmin adı
+                rivalcar.Size = new Size(52, 60);  // Resmin boyutları
+                rivalcar.BackColor = Color.Black;  // Arka plan rengini siyah yapıyoruz
+                rivalcar.Location = new Point(124, (-500 * i) - 60);  // Arabayı farklı başlangıç konumlarına yerleştiriyoruz
+                rivalcar.BackgroundImageLayout = ImageLayout.Stretch;  // Görselin boyutunu strech yapıyoruz
+                rivalcar.Visible = true;  // Resmi görünür yapıyoruz
+
+                // pictureBoxes dizisine rakip arabayı ekliyoruz
+                mainPanel1.Controls.Add(pictureBoxes[i]);
+
+                // Zamanlayıcıyı ayarlıyoruz
+                Timer RivalcarsTimer = new Timer();
+                RivalcarsTimer.Tag = "RivalcarTimer" + i;
+
+                RivalcarsTimer.Interval = 100;  // Zamanlayıcıyı 100 ms aralıklarla çalıştırıyoruz
+                RivalcarsTimer.Tick += new EventHandler(RivalcarsTimerTick);  // Zamanlayıcı tetiklendiğinde yapılacak işlem
+                RivalcarsTimer.Start();  // Zamanlayıcıyı başlatıyoruz
+            }
+        }
+
+        // Rakip arabaların hareketini kontrol eden zamanlayıcı tick eventi
+        public void RivalcarsTimerTick(object sender, EventArgs e)
+        {
+            // Gönderilen zamanlayıcıyı alıyoruz
+            Timer tm = (Timer)sender;
+
+            // Zamanlayıcı etiketinden araba numarasını çıkarıyoruz
+            int othercar = Convert.ToInt32(tm.Tag.ToString().Substring(15));
+
+            // Rakip arabaların yönünü rastgele belirliyoruz
+            int othercarRandom = ran.Next(0, 3);
+
+            if (othercarRandom == 0)
+            {
+                RivalCarsRight[othercar] = false;
+                RivalCarsLeft[othercar] = false;
+            }
+            else if (othercarRandom == 1)
+            {
+                RivalCarsRight[othercar] = false;
+                RivalCarsLeft[othercar] = true;
+            }
+            else if (othercarRandom == 2)
+            {
+                RivalCarsRight[othercar] = true;
+                RivalCarsLeft[othercar] = false;
+            }
+
+            // Eğer rakip araba sola gitmekteyse, sola kaydır
+            if (RivalCarsLeft[othercar] && pictureBoxes[othercar].Left > 30)
+            {
+                pictureBoxes[othercar].Left -= 5;
+            }
+            // Eğer rakip araba sağa gitmekteyse, sağa kaydır
+            if (RivalCarsRight[othercar] && pictureBoxes[othercar].Left < 218)
+            {
+                pictureBoxes[othercar].Left += 5;
+            }
+
+            // Rakip arabayı aşağı doğru hareket ettiriyoruz (yol boyunca)
+            pictureBoxes[othercar].Top -= 50 + arabaHızı;
+
+            // Eğer rakip araba kendi aracınızla çarpışırsa, hızınızı sıfırlayın
+            if (pictureBoxes[othercar].Bounds.IntersectsWith(pictureBox1.Bounds))
+            {
+                arabaHızı = 0;
+            }
+        }
+
+        // Araba hızını güncelleyen zamanlayıcı tick eventi
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Eğer araba çalışıyorsa ve hızı 80'e ulaşmadıysa, hızını arttır
+            if (arabaCalısıyorMu && arabaHızı < 80)
+            {
+                arabaHızı += 5;
+            }
+            // Eğer araba çalışmıyorsa ve hız sıfırlanmadıysa, hızını düşür
+            if (!arabaCalısıyorMu && arabaHızı > 0)
+            {
+                arabaHızı -= 5;
+            }
+            // Eğer fren yapılmışsa, hızını düşür
+            if (arabaBreak && arabaHızı > 0)
+            {
+                arabaHızı -= 5;
+            }
+            // Eğer hız sıfırsa, araba duruyor
+            if (arabaHızı == 0)
+            {
+                arabaDuruyorMu = true;
+            }
+            else
+            {
+                arabaDuruyorMu = false;
+            }
+        }
+
+        // Araba panellerini hareket ettiren zamanlayıcı tick eventi
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // Eğer araba durmuyorsa, panelleri hareket ettir
+            if (!arabaDuruyorMu)
+            {
+                Paneller();
+
+                // Araba sola gitmekteyse, sola kaydır
+                if (arabaLeft && pictureBox1.Left > 30)
+                {
+                    pictureBox1.Left -= 5;
+                }
+                // Araba sağa gitmekteyse, sağa kaydır
+                if (arabaRight && pictureBox1.Right < 218)
+                {
+                    pictureBox1.Left += 5;
+                }
+            }
+        }
+
+        // Klavye tuşlarına basıldığında yapılan işlemler
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                arabaBreak = false;  // Freni kaldır
+                arabaCalısıyorMu = true;  // Araba çalışmaya başlasın
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                arabaBreak = true;  // Freni çek
+                arabaCalısıyorMu = false;  // Araba durmaya başlasın
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                arabaRight = false;  // Sağ yönü kapat
+                arabaLeft = true;  // Sola gitmeye başla
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                arabaLeft = false;  // Sol yönü kapat
+                arabaRight = true;  // Sağa gitmeye başla
+            }
+        }
+
+        // Klavye tuşu bırakıldığında yapılan işlemler
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                arabaCalısıyorMu = false;  // Araba duracak
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                arabaBreak = false;  // Freni kaldır
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                arabaLeft = false;  // Sola gitmeyi durdur
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                arabaRight = false;  // Sağa gitmeyi durdur
+            }
+        }
+
+        // Panellerin hareketi (yol panelleri)
+        public void Paneller()
+        {
+            pictureBox2.Top = pictureBox2.Top + arabaHızı;  // Panelin yukarı doğru hareketi
+            pictureBox3.Top = pictureBox3.Top + arabaHızı;  // Diğer panelin yukarı doğru hareketi
+        }
+    }
+}
